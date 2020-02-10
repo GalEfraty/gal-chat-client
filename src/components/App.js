a43cd9588693652656579f0b1544092efbd4d09a
Gal Efraty <gal.efraty@gmail.com>
2020-02-10 16:28:08 +0200

    setup basic architecture

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,16 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import axios from "axios";
 
 function App() {
-  return <div>welcome to chat app</div>;
+  const [stamState, setStamState] = useState("")
+  useEffect(() => {
+    const fetchListData = async () => {
+      const result = await axios.get(`${process.env.REACT_APP_SERVER_URL}/api/hello`)
+      setStamState(result.data.message)
+    }
+    fetchListData()
+}, [])
+  return <div>welcome to chat app {stamState}</div>;
 }
 
 export default App;
